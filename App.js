f2ddf5fed47bcdcf541d1fe3e0e31c91bbdc2e09
eduuardo1357 <eduardo.silva1077@etec.sp.gov.br>
2024-03-14 08:22:34 -0300

    Button

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Title from './Title/';
 
 export default function App() {
@@ -9,6 +9,7 @@ export default function App() {
       <Text>Desenvolvimento de dispositivos moveis</Text>
       <Text>Aula dia 07-03-2024</Text>
       <Title/>
+      <Button title='Salvar'> </Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -22,3 +23,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+  
